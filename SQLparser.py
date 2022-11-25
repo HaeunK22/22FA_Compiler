0fb322a1e92a47b9fd8c96f016a848640d93a310
HaeunK22 <69032913+HaeunK22@users.noreply.github.com>
2022-11-25 22:11:08 +0900

    Update parser

--- SQLparser.py
+++ SQLparser.py
@@ -79,61 +79,97 @@ lexer = lex()
 
 # Write functions for each grammar rule which is
 # specified in the docstring.
+# def p_expression(p):
+#     '''
+#     expression : term PLUS term
+#                | term MINUS term
+#     '''
+#     # p is a sequence that represents rule contents.
+#     #
+#     # expression : term PLUS term
+#     #   p[0]     : p[1] p[2] p[3]
+#     # 
+#     p[0] = ('binop', p[2], p[1], p[3])
+
+# def p_expression_term(p):
+#     '''
+#     expression : term
+#     '''
+#     p[0] = p[1]
+
 def p_expression(p):
     '''
-    expression : term PLUS term
-               | term MINUS term
+    expression : DB DOT TABLE DOT INSERT LPAREN LCURL term RCURL RPAREN
     '''
-    # p is a sequence that represents rule contents.
-    #
-    # expression : term PLUS term
-    #   p[0]     : p[1] p[2] p[3]
-    # 
-    p[0] = ('binop', p[2], p[1], p[3])
 
-def p_expression_term(p):
-    '''
-    expression : term
-    '''
-    p[0] = p[1]
+    # NEED FIX
+
+# def p_term(p):
+#     '''
+#     term : factor TIMES factor
+#         | factor DIVIDE factor
+#     '''
+#     p[0] = ('binop', p[2], p[1], p[3])
+
+# def p_term_factor(p):
+#     '''
+#     term : factor
+#     '''
+#     p[0] = p[1]
 
 def p_term(p):
     '''
-    term : factor TIMES factor
-         | factor DIVIDE factor
+    term : KEY COLON value
+        | term COMMA term
     '''
-    p[0] = ('binop', p[2], p[1], p[3])
 
-def p_term_factor(p):
-    '''
-    term : factor
-    '''
-    p[0] = p[1]
+    # NEED FIX
 
-def p_factor_number(p):
-    '''
-    factor : NUMBER
-    '''
-    p[0] = ('number', p[1])
+# def p_factor_number(p):
+#     '''
+#     factor : NUMBER
+#     '''
+#     p[0] = ('number', p[1])
 
-def p_factor_name(p):
-    '''
-    factor : NAME
-    '''
-    p[0] = ('name', p[1])
+# def p_factor_name(p):
+#     '''
+#     factor : NAME
+#     '''
+#     p[0] = ('name', p[1])
 
-def p_factor_unary(p):
-    '''
-    factor : PLUS factor
-           | MINUS factor
-    '''
-    p[0] = ('unary', p[1], p[2])
+# def p_factor_unary(p):
+#     '''
+#     factor : PLUS factor
+#            | MINUS factor
+#     '''
+#     p[0] = ('unary', p[1], p[2])
 
-def p_factor_grouped(p):
+# def p_factor_grouped(p):
+#     '''
+#     factor : LPAREN expression RPAREN
+#     '''
+#     p[0] = ('grouped', p[2])
+
+def p_value_char(p):
     '''
-    factor : LPAREN expression RPAREN
+    value : CHARACTER
     '''
-    p[0] = ('grouped', p[2])
+
+    # NEED FIX
+
+def p_value_int(p):
+    '''
+    value : INTEGER
+    '''
+
+    # NEED FIX
+
+def p_value_char(p):
+    '''
+    value : FLOAT
+    '''
+
+    # NEED FIX
 
 def p_error(p):
     print(f'Syntax error at {p.value!r}')
