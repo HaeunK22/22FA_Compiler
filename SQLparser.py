7ab1aefec50222e22127083e2baaae891f2643b5
HaeunK22 <69032913+HaeunK22@users.noreply.github.com>
2022-11-25 22:35:31 +0900

    Delete example code

--- SQLparser.py
+++ SQLparser.py
@@ -1,23 +1,4 @@
 # -----------------------------------------------------------------------------
-# example.py
-#
-# Example of using PLY To parse the following simple grammar.
-#
-#   expression : term PLUS term
-#              | term MINUS term
-#              | term
-#
-#   term       : factor TIMES factor
-#              | factor DIVIDE factor
-#              | factor
-#
-#   factor     : NUMBER
-#              | NAME
-#              | PLUS factor
-#              | MINUS factor
-#              | LPAREN expression RPAREN
-#
-# -----------------------------------------------------------------------------
 # SQLparser.py
 #
 #   expression : DB DOT TABLE DOT INSERT LPAREN LCURL term RCURL RPAREN
@@ -35,24 +16,15 @@ from ply.yacc import yacc
 # --- Tokenizer
 
 # All tokens must be named in advance.
-# tokens = ( 'PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'LPAREN', 'RPAREN',
-#           'NAME', 'NUMBER' )
 tokens = ('CHARACTER', 'INTEGER', 'FLOAT', 'KEY', 'COLON', 'LCURL', 'RCURL', 'LPAREN', 'RPAREN', 'COMMA', 'INSERT', 'DB', 'TABLE', 'DOT')
 
 # Ignored characters
 t_ignore = ' \t'
 
 # Token matching rules are written as regexs
-# t_PLUS = r'\+'
-# t_MINUS = r'-'
-# t_TIMES = r'\*'
-# t_DIVIDE = r'/'
-# t_LPAREN = r'\('
-# t_RPAREN = r'\)'
-# t_NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
 t_CHARACTER = r'[a-zA-Z_][a-zA-Z_]*'
-t_INTEGER = r'[]'
-t_FLOAT = r''
+t_INTEGER = r'[]'   # NEED FIX
+t_FLOAT = r''   # NEED FIX
 t_KEY = r'[a-zA-Z_][a-zA-Z_]*'
 t_COLON = r'\:'
 t_LCURL = r'\{'
@@ -67,10 +39,6 @@ t_DOT = r'\.'
 
 # A function can be used if there is an associated action.
 # Write the matching regex in the docstring.
-# def t_NUMBER(t):
-#     r'\d+'
-#     t.value = int(t.value)
-#     return t
 
 # Ignored token with an action associated with it
 def t_ignore_newline(t):
@@ -89,43 +57,12 @@ lexer = lex()
 
 # Write functions for each grammar rule which is
 # specified in the docstring.
-# def p_expression(p):
-#     '''
-#     expression : term PLUS term
-#                | term MINUS term
-#     '''
-#     # p is a sequence that represents rule contents.
-#     #
-#     # expression : term PLUS term
-#     #   p[0]     : p[1] p[2] p[3]
-#     # 
-#     p[0] = ('binop', p[2], p[1], p[3])
-
-# def p_expression_term(p):
-#     '''
-#     expression : term
-#     '''
-#     p[0] = p[1]
-
 def p_expression(p):
     '''
     expression : DB DOT TABLE DOT INSERT LPAREN LCURL term RCURL RPAREN
     '''
 
     # NEED FIX
-
-# def p_term(p):
-#     '''
-#     term : factor TIMES factor
-#         | factor DIVIDE factor
-#     '''
-#     p[0] = ('binop', p[2], p[1], p[3])
-
-# def p_term_factor(p):
-#     '''
-#     term : factor
-#     '''
-#     p[0] = p[1]
 
 def p_term(p):
     '''
@@ -134,31 +71,6 @@ def p_term(p):
     '''
 
     # NEED FIX
-
-# def p_factor_number(p):
-#     '''
-#     factor : NUMBER
-#     '''
-#     p[0] = ('number', p[1])
-
-# def p_factor_name(p):
-#     '''
-#     factor : NAME
-#     '''
-#     p[0] = ('name', p[1])
-
-# def p_factor_unary(p):
-#     '''
-#     factor : PLUS factor
-#            | MINUS factor
-#     '''
-#     p[0] = ('unary', p[1], p[2])
-
-# def p_factor_grouped(p):
-#     '''
-#     factor : LPAREN expression RPAREN
-#     '''
-#     p[0] = ('grouped', p[2])
 
 def p_value_char(p):
     '''
